058d35fb54cd7e6569056a00f981929baca99532
koranins <ksu@earnest.com>
2018-04-14 13:19:06 -0700

    Add unique hash to the ES5 output file name.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HashOutput = require('webpack-plugin-hash-output');
 
 module.exports = {
   entry: './js/main.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -21,6 +22,7 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   plugins: [
-    new HtmlWebpackPlugin({template: path.resolve(__dirname, 'js/index.html')})
+    new HtmlWebpackPlugin({template: path.resolve(__dirname, 'js/index.html')}),
+    new HashOutput()
   ]
 };
