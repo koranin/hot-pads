24ca664050b0a2861add3e689c60bb4df83f428c
koranins <ksu@earnest.com>
2018-04-16 19:04:14 -0700

    Rename images variable to photos

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PhotoGallery from './components/PhotoGallery';
 
-const images = [
+const photos = [
   { url: 'url1', caption: 'cap1' },
   { url: 'url2', caption: 'cap2' },
   { url: 'url3', caption: 'cap3' }
 ];
 
 ReactDOM.render(
-  <PhotoGallery images={images} />, document.getElementById('app')
+  <PhotoGallery photos={photos} />, 
+  document.getElementById('app')
 );
